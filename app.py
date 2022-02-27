163d758d9be0bc7252c4ae25b49f3df3745fec1a
Mauricio Alvarado <mau.alvalop43@gmail.com>
2022-02-26 18:07:44 -0600

    Client-Server Architecture

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
+from os import environ
 from flask import Flask, request, render_template
 from flask_cors import CORS
 from bot import Bot
 from database import DB
-
 
 database = DB()
 bot = Bot(database)
 app = Flask(__name__, static_folder="static", template_folder="templates")
 cors = CORS(app, resources={r"*": {"origins": "*"}}, supports_credentials=True)
 
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
 
 @app.route("/resetdb")
 def reset_db():
     database.init_data()
     return "Se ha reseteado la base de datos"
 
-@app.route("/bot",  methods=['GET'])
+
+@app.route("/bot", methods=['GET'])
 def bot_request():
     text = request.args.get('msg')
     return {
         "message": str(bot.get_response(text))
     }
+
+
+app.run(debug=False, port=environ.get("PORT", 5000), host='0.0.0.0')
